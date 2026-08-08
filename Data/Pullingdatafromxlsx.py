9b3e7584bf00353407fc9cfcd17f26e2939779d5
BroederZ <65283016+BroederZ@users.noreply.github.com>
2026-08-09 01:04:24 +0200

    Update data processing script to save cumulative scores plot and remove old plot file

--- Data/Pullingdatafromxlsx.py
+++ Data/Pullingdatafromxlsx.py
@@ -1,10 +1,13 @@
 from pathlib import Path
 import matplotlib.pyplot as plt
+from numpy import save
 import pandas as pd
 #import openpyxl
 
 excel_path = Path(__file__).resolve().with_name("Catan_ostuni.xlsx")
 file = pd.read_excel(excel_path, sheet_name="Blad1", engine="openpyxl")
+print("excel_path:", excel_path)
+print("file:", file)
 
 #x = file["Max"].tolist() #Need to make it a index(.values) or list(.tolist()) from it
 #y = file["Enzo"].tolist()
@@ -12,28 +15,34 @@ file = pd.read_excel(excel_path, sheet_name="Blad1", engine="openpyxl")
 #plt.plot(x, y, marker = 'o')
 #print(plt.plot(x, y, marker = 'o'))
 #plt.show()
+
 # Get all columns (except index/names)
 data = file[["Max", "Enzo", "Antoine", "Ma", "Pa"]]
+print("data:", data)
 
 # Plot all
 for column in data.columns:
-    plt.plot(data[column].cumsum(), label=column)
-
-
+   plt.plot(data[column].cumsum(), label=column)
+   print(data[column].cumsum())
 
 # Calculate cumulative sum (running total)
-y1_cumsum = 1.cumsum()
-y2_cumsum = 2.cumsum()
+#y1_cumsum = 1.cumsum()
+#y2_cumsum = 2.cumsum()
 
 # Plot
 plt.figure(figsize=(10, 6))
-plt.plot(y1_cumsum, label="Max", marker='o')
-plt.plot(y2_cumsum, label="Enzo", marker='h')
+plt.plot(data.columns, label="Max", marker='o')
+plt.plot(data.columns, label="Enzo", marker='h')
 plt.xlabel("Game Number")#verander naar datum
 plt.ylabel("Cumulative Score")
 plt.title("Catan Scores - Cumulative")
 plt.legend()
 #plt.grid(True)
-plt.savefig("catan_scores.png", dpi=300, bbox_inches="tight")
-#save naar de data map
-print("Plot saved as catan_scores.png")
+
+#save naar de Graphs map in de Data map
+save_path = Path(__file__).resolve().parent / "Graphs"
+#print("save_path:", save_path) # for checking the path
+#save_path.mkdir(exist_ok=True) # To remove the error codes; it makes the map
+save_path = save_path / "catan_totalscores.png"
+plt.savefig(save_path, dpi=300, bbox_inches="tight")
+print("Plot saved as catan_totalscores.png in path:", save_path)
