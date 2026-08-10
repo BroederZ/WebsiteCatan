29c3e33c397c823105f1ff2fa479e1cff2762345
BroederZ <65283016+BroederZ@users.noreply.github.com>
2026-08-10 16:11:40 +0200

    Refactor data processing script to improve cumulative score plotting and save updated graph

--- Data/Pullingdatafromxlsx.py
+++ Data/Pullingdatafromxlsx.py
@@ -4,39 +4,28 @@ from numpy import save
 import pandas as pd
 #import openpyxl
 
+#open path to the excel file
 excel_path = Path(__file__).resolve().with_name("Catan_ostuni.xlsx")
+#print("excel_path:", excel_path)
+#leest de hele file
 file = pd.read_excel(excel_path, sheet_name="Blad1", engine="openpyxl")
-print("excel_path:", excel_path)
-print("file:", file)
-
-#x = file["Max"].tolist() #Need to make it a index(.values) or list(.tolist()) from it
-#y = file["Enzo"].tolist()
-#print(x,y)
-#plt.plot(x, y, marker = 'o')
-#print(plt.plot(x, y, marker = 'o'))
-#plt.show()
+#print("file:", file)
 
 # Get all columns (except index/names)
 data = file[["Max", "Enzo", "Antoine", "Ma", "Pa"]]
+#data["Datum"] = pd.to_datetime(data["Datum"])  # Convert 'Datum' to datetime
 print("data:", data)
 
-# Plot all
-for column in data.columns:
-   plt.plot(data[column].cumsum(), label=column)
-   print(data[column].cumsum())
-
-# Calculate cumulative sum (running total)
-#y1_cumsum = 1.cumsum()
-#y2_cumsum = 2.cumsum()
-
-# Plot
+# Plot all cumulative scores
 plt.figure(figsize=(10, 6))
-plt.plot(data.columns, label="Max", marker='o')
-plt.plot(data.columns, label="Enzo", marker='h')
-plt.xlabel("Game Number")#verander naar datum
-plt.ylabel("Cumulative Score")
+for column in data.select_dtypes(include=['number']).columns:
+   plt.plot(data[column].cumsum(), label=column, marker='o')#Elke lijn een specifiek marker geven
+   #print(data[column].cumsum())
+plt.xlabel("Datum")#verander naar datum in het figuur zelf
+plt.ylabel("Totaal behaalde punten")
 plt.title("Catan Scores - Cumulative")
 plt.legend()
+
 #plt.grid(True)
 
 #save naar de Graphs map in de Data map
@@ -45,4 +34,5 @@ save_path = Path(__file__).resolve().parent / "Graphs"
 #save_path.mkdir(exist_ok=True) # To remove the error codes; it makes the map
 save_path = save_path / "catan_totalscores.png"
 plt.savefig(save_path, dpi=300, bbox_inches="tight")
+#plt.show() #FigureCanvassAgg is non-interactive dus word niet getoond.
 print("Plot saved as catan_totalscores.png in path:", save_path)
